38e1a8d95e611992c06f596f961e413a0fc58f61
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2024-10-18 14:22:08 +0200

    update file and SuperAdminController

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,14 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/profil', name: 'profil.')]
 class ProfilController extends AbstractController
 {
+
     #[Route('', name: 'index')]
     public function index(): Response
     {
+//        if (!$this->isGranted("ROLE_ADMIN")) {
+//            return $this->redirectToRoute("home.index");
+//        }
 
+        if($this->isGranted('ROLE_ADMIN')) {
+            $this->addFlash('success', 'Bienvenue sur le profil '.$this->getUser()->getEmail());
+        }
+        return $this->render('profil/index.html.twig', [
+
+        ]);
+    }
+    #[IsGranted("ROLE_ADMIN")]
+    public function modifierProfil(): Response
+    {
+        if (!$this->isGranted("ROLE_ADMIN")) {
+            return $this->redirectToRoute("home.index");
+        }
         return $this->render('profil/index.html.twig', [
 
         ]);
